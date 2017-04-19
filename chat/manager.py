36225d2b38899fa45438698d04fe23e30a5e9eaf
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-04-19 19:58:57 +0200

    fix django 1.7-1.10 migration

--- chat/manager.py
+++ chat/manager.py
@@ -1,25 +1,21 @@
 import django
+from django.db.models import Manager
 
 from .query import MessageQuerySet
 
 if django.VERSION < (1, 7):
-    from django.db.models.manager import Manager
-
     class MessageManager(Manager):
         """Message manager"""
-
         def get_queryset(self):
             return MessageQuerySet(self.model, using=self._db)
 
-        # Django 1.4 & 1.5 compatible
-        get_query_set = get_queryset
+        if django.VERSION < (1, 6):
+            get_query_set = get_queryset
 
         def undelivered(self, to=None):
             """Fetch only undelivered messages"""
             return self.get_queryset().undelivered(to)
 else:
-    from django.db.models.manager import BaseManager
-
-    class MessageManager(BaseManager.from_queryset(MessageQuerySet)):
+    class MessageManager(Manager.from_queryset(MessageQuerySet)):
         """Message manager"""
         pass
