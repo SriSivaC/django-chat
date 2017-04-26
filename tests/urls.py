0bbea10640ec741caa15551a8fa538b13317ec22
Tomasz Jakub Rup <tomasz.rup@gmail.com>
2017-04-26 22:08:13 +0200

    fix admin site config

--- tests/urls.py
+++ tests/urls.py
@@ -1,11 +1,10 @@
 import django
 from django.contrib import admin
 
+admin.autodiscover()
 
 if django.VERSION[:2] < (1, 10):
     from django.conf.urls import patterns, include
-
-    admin.autodiscover()
 
     urlpatterns = patterns('', (r'^admin/', include(admin.site.urls)))
 else:
